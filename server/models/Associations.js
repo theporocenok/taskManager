f4b1599c845914ea4b8b493118d5487e268b06b1
theporocenok <porocenok@bk.ru>
2021-04-27 21:14:23 +0500

    Associations: получение списка всех задач подчинённых

--- server/models/Associations.js
+++ server/models/Associations.js
@@ -1,11 +1,19 @@
 import Task from './Task.js';
 import User from './User.js';
 
-User.hasMany(Task, {foreignKey: 'creatorId', onUpdate: 'CASCADE'});
-User.hasMany(Task, {foreignKey: 'responsibleId', onUpdate: 'CASCADE'});
-User.belongsTo(User, {foreignKey: 'leaderId'});
+User.hasMany(Task, {foreignKey: 'creatorId', sourceKey: 'id', onUpdate: 'CASCADE'});
+User.hasMany(Task, {foreignKey: 'responsibleId', sourceKey: 'id', onUpdate: 'CASCADE'});
+User.belongsTo(User, {foreignKey: 'leaderId', targetKey: 'id',});
 
-Task.belongsTo(User, {foreignKey: 'creatorId'});
-Task.belongsTo(User, {foreignKey: 'responsibleId'});
+Task.belongsTo(User, {foreignKey: 'creatorId', targetKey: 'id', });
+Task.belongsTo(User, {foreignKey: 'responsibleId', targetKey: 'id', });
 
+User.prototype.getSubordinatesTasks = async function(filters = {}) {
+  return Task.getAllByUserIds(
+    (await this.getSubordinates()).map(item => item.id),
+    filters
+  );
+}
+
+global.sequelize.sync();
 export default {User, Task};
