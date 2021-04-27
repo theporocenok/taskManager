ae93a1726dfa1f7e29253900edbcee103c1df9db
theporocenok <porocenok@bk.ru>
2021-04-27 21:12:59 +0500

    TaskController: создание задачи

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -2,18 +2,39 @@ import Associations from './../models/Associations.js';
 
 let Task = Associations.Task;
 
-export function getAll(req, res, next) {
+let errHandler = (e) => {
+  console.error('Error', e);
+}
+
+export async function getAll(req, res, next) {
+  let user = res.locals.user;
   res.status(200).json({
-    message: '',
+    message: await user.getSubordinatesTasks(req.body.filters),
   });
   next();
 }
 
-export function create(req, res, next) {
-  res.status(200).json({
-    message: 'temp message for tasks post',
-  });
-  next();
+export async function create(req, res, next) {
+  try {
+    let creator = res.locals.user;
+    let task = await Task.create({
+      title: req.body.title,
+      description: req.body.description,
+      expirationDate: req.body.expirationDate,
+      priority: req.body.priority,
+      status: req.body.status,
+      creatorId: creator.dataValues.id,
+      responsibleId: req.body.responsibleId,
+    })
+      .catch(errHandler);
+    if (!!task) {
+      res.status(201).send();
+    }
+    next();
+  }catch (e) {
+    res.status(400);
+    next(e);
+  }
 }
 
 export function update(req, res, next) {
