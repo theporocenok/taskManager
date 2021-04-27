d02b429141190c588bd6dd8034022d8382fe0421
theporocenok <porocenok@bk.ru>
2021-04-27 21:15:27 +0500

    Пути для /users

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import sequelize from "./database/connection.js";
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import userRoutes from './routes/user.js';
 import authRoutes from './routes/auth.js';
 import taskRoutes from './routes/task.js';
 import AuthMiddleware from "./middlewares/AuthMiddleware.js";
@@ -15,6 +16,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/users', AuthMiddleware, userRoutes);
 app.use('/api/task', AuthMiddleware, taskRoutes);
 
 export default app;
