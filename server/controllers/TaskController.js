60129bf18a7087f4ab5d791b6346885360f4a421
theporocenok <porocenok@bk.ru>
2021-04-29 12:43:16 +0500

    TaskController: функции удаления и обновления

--- server/controllers/TaskController.js
+++ server/controllers/TaskController.js
@@ -9,7 +9,11 @@ let errHandler = (e) => {
 export async function getAll(req, res, next) {
   let user = res.locals.user;
   res.status(200).json({
-    data: await user.getSubordinatesTasks(req.body.filters),
+    data: await user.getSubordinatesTasks({
+      dateFrom: req.query.dateFrom,
+      dateTo: req.query.dateTo,
+      subordinate: req.query.subordinate
+    }),
   });
   next();
 }
@@ -37,18 +41,48 @@ export async function create(req, res, next) {
   }
 }
 
-export function update(req, res, next) {
-  res.status(200).json({
-    message: 'temp message for tasks update',
-  });
-  next();
+export async function update(req, res, next) {
+  try {
+    let creator = res.locals.user;
+    let task = await Task.update({
+      title: req.body.title,
+      description: req.body.description,
+      expirationDate: req.body.expirationDate,
+      priority: req.body.priority,
+      status: req.body.status,
+      creatorId: creator.dataValues.id,
+      responsibleId: req.body.responsibleId,
+    }, {
+      where: {
+        id: req.params.id
+      }
+    })
+      .catch(errHandler);
+    if (!!task) {
+      res.status(204).send();
+    }
+    next();
+  }catch (e) {
+    res.status(400);
+    next(e);
+  }
 }
 
-export function remove(req, res, next) {
-  res.status(200).json({
-    message: 'temp message for tasks remove',
-  });
-  next();
+export async function remove(req, res, next) {
+  try {
+    await Task.destroy( {
+      where: {
+        id: req.params.id
+      }
+    })
+      .catch(errHandler);
+
+    res.status(204).send();
+    next();
+  }catch (e) {
+    res.status(400);
+    next(e);
+  }
 }
 
 export default {getAll, create, update, remove};
